eea6389c6c3573a22f393a30bfdb93df0ce2176f
Matheus <mllcarvalho.it@gmail.com>
2023-03-23 15:30:57 -0300

    modulo iam

--- locals.tf
+++ locals.tf
@@ -8,14 +8,3 @@ locals {
     ]
   ])
 }
-
-output "attach" {
-  value = flatten([
-    for k, v in var.iam_roles : [
-      for r in v.attached_policies : {
-        attach  = r
-        role = k
-      }
-    ]
-  ])
-}
